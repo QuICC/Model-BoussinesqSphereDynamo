7f9449469ada34e62ae65f4a147978c700eff4bb
Philippe Marti <philippe.marti@erdw.ethz.ch>
2022-06-08 18:06:26 +0200

    Update benchmark

--- TestSuite/validate_benchmark_Explicit.py
+++ TestSuite/validate_benchmark_Explicit.py
@@ -8,8 +8,7 @@ results = []
 
 # Tolerance per max rows
 rows = list(range(0, 101, 10))
-tols = [51, 61, 81, 141, 201, 261, 301, 331, 451, 491, 501] # Without n spectra
-tols = [61, 81, 111, 191, 221, 271, 311, 381, 470, 541, 571] # With n spectra
+tols = [26, 64, 82, 123, 153, 162, 203, 204, 254, 255, 305]
 
 prefixes = ['temperature', 'kinetic', 'magnetic']
 spectra = ['l', 'm', 'n']
@@ -18,12 +17,12 @@ spectra = ['l', 'm', 'n']
 for prefix in prefixes:
     # Energy
     for r, t in zip(rows,tols):
-        results.append(vt.tableTest(prefix + '_energy.dat', ref_dir, data_dir, r, tol = t, max_rows = r+1))
+        results.append(vt.tableTest(prefix + '_energy.dat', ref_dir, data_dir, r, tol = t, max_rows = r+1, perrow = True, max_firstcol = 1))
 
     # Spectra
     for mode in spectra:
         for r, t in zip(rows,tols):
-            results.append(vt.tableTest(prefix +  '_' + mode + f'_spectrum{r:04}.dat', ref_dir, data_dir, r, tol = t, percol = True))
+            results.append(vt.tableTest(prefix +  '_' + mode + f'_spectrum{r:04}.dat', ref_dir, data_dir, r, tol = t, percol = True, perrow = True, max_firstcol = 1))
 
 # Nusselt number
 for r, t in zip(rows,tols):
