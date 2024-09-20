20cd66a94f2b4b833ce7109f76deed5cbafd4c6d
Philippe Marti <philippe.marti@erdw.ethz.ch>
2024-09-20 10:38:24 +0200

    Address review

--- TestSuite/validate_benchmark_Explicit_split_equation.py
+++ TestSuite/validate_benchmark_Explicit_split_equation.py
@@ -32,9 +32,5 @@ for r, t in zip(rows,tols):
 for r, t in zip(rows,tols):
     results.append(vt.tableTest("cfl.dat", ref_dir, data_dir, r, usecols=(0,1,3,5,7,8,9,10,11), tol = t, max_rows = r+1))
 
-# Angular momentum
-#for r, t in zip(rows,tols):
-#    results.append(vt.tableTest("angular_momentum.dat", ref_dir, data_dir, r, tol = t, max_rows = r+1))
-
 # Output test summary
 vt.printSummary(results, rows, tols)
